a3e95f8e9669056a04a5bba431ba6d13c805a464
Natanael <natanael.gonzales@gmail.com>
2021-03-29 11:31:29 -0300

    controllers basic structure

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PrincipalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, seja bem vindo ao curso';
-});
+Route::get('/', 'PrincipalController@principal');
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós';
-});
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/contato', 'ContatoController@contato');
